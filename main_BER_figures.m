7f806508e50db9daa7107d0460d8dc9999347baa
felipescoelho <luizfscoelho.92@gmail.com>
2022-10-17 21:47:32 -0300

    changes everywhere

--- main_BER_figures.m
+++ main_BER_figures.m
@@ -13,35 +13,167 @@ fprintf('Starting main_BER_figures.m ... \n\n')
 fprintf('This script generates figures from the BER calculation.\n')
 
 % Definitions
+global settingsFileName resultsFolder figuresSurface
+settingsFileName = 'settingsData.mat';
 resultsFolder = 'ber_results';
 figuresFolder = [resultsFolder '/figures'];
 figuresByCPFolder = [figuresFolder '/by_cyclic_prefix'];
-figuresByTypeFolder = [filguresFolder '/by_type'];
+figuresSurface = [figuresFolder '/surface'];
 if ~isfolder(figuresFolder)
     mkdir(figuresFolder)
 end
-if ~isfolder(figureByCPsFolder)
+if ~isfolder(figuresByCPFolder)
     mkdir(figuresByCPFolder)
 end
-if ~isfolder(figuresByTypeFolder)
-    mkdir(figuresByTypeFolder)
+if ~isfolder(figuresSurface)
+    mkdir(figuresSurface)
 end
 resultFiles = dir(fullfile(resultsFolder));
 typeOFDMSet = {'wtx' 'wrx' 'WOLA' 'CPW' 'CPwtx' 'CPwrx'};
+settingsLoader = load(settingsFileName);
+cpLengthVector = settingsLoader.settingsData.generalSettings.cyclicPrefix;
+snrValues = settingsLoader.settingsData.generalSettings.snrValues;
+
 for typeOFDMIndex = 1:length(typeOFDMSet)
     typeOFDM = typeOFDMSet{typeOFDMIndex};
-    filesByType = file_by_type(resultFiles, typeOFDM);
-    cpList = list_cp(filesByType);
+    [rcWindowResults, optWindowResults] = read_results(resultFiles, ...
+        typeOFDM);
+    plot_ber_surf(rcWindowResults, optWindowResults, typeOFDM)
 end
 
 
-function plot_ber_by_cp(cyclicPrefix, folderPath)
+function plot_ber_surf(rcWindowResults, optWindowResults, typeOFDM)
+
+global figuresSurface settingsFileName
+
+% Definitions
+width = 8.89;
+height = width*2/(1+sqrt(5));
+fontSize = 10;
+horizontalLeftDistance = .75;
+verticalBottomDistance = .85;
+plotWidth = width/2 - 2*horizontalLeftDistance;
+plotHeight = height - 2*verticalBottomDistance;
+settingsLoader = load(settingsFileName);
+cyclicPrefixVector = settingsLoader.settingsData.generalSettings.cyclicPrefix;
+snrVector = settingsLoader.settingsData.generalSettings.snrValues;
+switch typeOFDM
+    case {'wtx', 'wrx', 'CPwrx', 'CPwtx'}
+        fig = figure;
+        fig.Name = strcat(typeOFDM, ' BER Surface');
+        fig.Color = 'w';
+        fig.Units = 'centimeters';
+        fig.Position = [10 10 width height];
+        ax1 = axes('units', 'centimeters', 'position', ...
+            [horizontalLeftDistance+.5, verticalBottomDistance, ...
+            plotWidth, plotHeight]);
+        ax2 = axes('units', 'centimeters', 'position', ...
+            [width/2 + 1.5*horizontalLeftDistance, verticalBottomDistance, ...
+            plotWidth, plotHeight]);
+
+        subplot(ax1)
+        surf(cyclicPrefixVector, snrVector, ...
+            10*log10(optWindowResults.'), 'LineWidth', .1, ...
+            'EdgeAlpha', .91)
+        set(ax1, 'Ydir', 'reverse')
+        set(ax1, 'Xdir', 'reverse')
+        set(ax1, 'FontSize', fontSize)
+        set(ax1, 'TickLabelInterpreter', 'latex')
+        set(ax1, 'linewidth', 1)
+        ylim([-30 50])
+        xLabelAx1 = xlabel('CP Length', 'interpreter', 'latex', ...
+            'FontSize', fontSize, 'Color', 'k');
+        xLabelAx1.Units = 'centimeters';
+        xLabelAx1.Position(1) = xLabelAx1.Position(1) - .6;
+        xLabelAx1.Position(2) = xLabelAx1.Position(2) - .1;
+        xLabelAx1.Rotation = 22.5;
+        yLabelAx1 = ylabel('SNR, dB', 'interpreter', 'latex', ...
+            'FontSize', fontSize, 'Color', 'k');
+        yLabelAx1.Units = 'centimeters';
+        yLabelAx1.Position(1) = yLabelAx1.Position(1) + .5;
+        yLabelAx1.Position(2) = yLabelAx1.Position(2) - .1;
+        yLabelAx1.Rotation = -37.5;
+        zlabel('BER, dB', 'interpreter', 'latex', 'FontSize', fontSize, ...
+            'Color', 'k')
+        title('(a)', 'interpreter', 'latex', 'FontSize', fontSize, ...
+            'Color', 'k')
+        
+        subplot(ax2)
+        surf(cyclicPrefixVector, snrVector, ...
+            10*log10(rcWindowResults.'), 'LineWidth', .1, 'EdgeAlpha', .91)
+        set(ax2, 'Ydir', 'reverse')
+        set(ax2, 'Xdir', 'reverse')
+        set(ax2, 'FontSize', fontSize)
+        set(ax2, 'TickLabelInterpreter', 'latex')
+        set(ax2, 'linewidth', 1)
+        ylim([-30 50])
+%         xLabelAx2 = xlabel('CP Length', 'interpreter', 'latex', ...
+%             'FontSize', fontSize, 'Color', 'k');
+%         xLabelAx2.Units = 'centimeters';
+%         xLabelAx2.Position(1) = xLabelAx2.Position(1) - .6;
+%         xLabelAx2.Position(2) = xLabelAx2.Position(2) - .1;
+%         xLabelAx2.Rotation = 22.5;
+%         yLabelAx2 = ylabel('SNR, dB', 'interpreter', 'latex', ...
+%             'FontSize', fontSize, 'Color', 'k');
+%         yLabelAx2.Units = 'centimeters';
+%         yLabelAx2.Position(1) = yLabelAx2.Position(1) + .5;
+%         yLabelAx2.Position(2) = yLabelAx2.Position(2) - .1;
+%         yLabelAx2.Rotation = -37.5;
+        title('(b)', 'interpreter', 'latex', 'FontSize', fontSize, ...
+            'Color', 'k')
+        
+        fileName = strcat('ber_surface_', typeOFDM);
+        saveas(fig, [figuresSurface '/' fileName '.eps'], 'epsc')
+        saveas(fig, [figuresSurface '/' fileName '.fig'])
+    case {'WOLA', 'CPW'}
+        caseSet = {'1stepTx', '1stepRx', '2stepTx', '2stepRx', ...
+            '3stepTx', '3stepRx'};
+        for setIndex = 1:6
+            fig = figure;
+            fig.Name = strcat(typeOFDM, ' BER Surface ', ...
+                caseSet{setIndex});
+            fig.Color = 'w';
+            fig.Units = 'centimeters';
+            fig.Position = [10 10 width height];
+            ax1 = axes('units', 'centimeters', 'position', ...
+                [horizontalLeftDistance+.5, verticalBottomDistance, ...
+                plotWidth, plotHeight]);
+            ax2 = axes('units', 'centimeters', 'position', ...
+                [width/2 + horizontalLeftDistance, ...
+                verticalBottomDistance, plotWidth, plotHeight]);
+
+            subplot(ax1)
+            surf(cyclicPrefixVector, snrVector, ...
+                10*log10(optWindowResults(:, :, setIndex).'), ...
+                'LineWidth', .1, 'EdgeAlpha', .91)
+
+            subplot(ax2)
+            surf(cyclicPrefixVector, snrVector, ...
+                10*log10(rcWindowResults.'), 'LineWidth', .1, ...
+                'EdgeAlpha', .91)
+            
+            fileName = strcat('ber_surface_', typeOFDM, '_', ...
+                caseSet{setIndex});
+            saveas(fig, [figuresSurface '/' fileName '.eps'], 'epsc')
+            saveas(fig, [figuresSurface '/' fileName '.fig'])
+        end
+end
+end
+
+
+function plot_ber_cut_cp(rcWindowResults, optWindowResults, cpLength, ...
+    typeOFDM, folderPath)
 % Funtion to plot different systems for a single CP length.
 %
 % - Input
-%   . cyclicPrefix: Lenght of the cyclic prefix
-%   . folderPath: Where the figure will be saved
+%   . rcWindowResults: Matrix (CP x SNR) with results for RC window
+%   . optWindowResults: Matrix (CP x SNR) with results for optimal window
+%   . cpLength: Number of samples in cyclic prefix
+%   . folderPath: Folder to save figure
 
+global settingsFileName
+settingsLoader = load(settingsFileName);
+snrValues = settingsLoader.settingsData.generalSettings.snrValues;
 width = 8.99;
 height = 2*width / (1+sqrt(5));
 fontSize = 11;
@@ -62,6 +194,58 @@ subplot(ax1)
 end
 
 
+function [rcWindowResults, optWindowResults] = read_results( ...
+    resultFiles, typeOFDM)
+global settingsFileName resultsFolder
+settingsLoader = load(settingsFileName);
+cpLengthVector = settingsLoader.settingsData.generalSettings.cyclicPrefix;
+snrValues = settingsLoader.settingsData.generalSettings.snrValues;
+filesByType = file_by_type(resultFiles, typeOFDM);
+[rcFiles, optFiles] = separate_rc_opt(filesByType);
+switch typeOFDM
+    case {'wtx', 'wrx', 'CPwtx', 'CPwrx'}
+        rcWindowResults = zeros(length(cpLengthVector), length(snrValues));
+        optWindowResults = zeros(length(cpLengthVector), length(snrValues));
+        for cpIndex = 1:length(cpLengthVector)
+            rcDataLoader = load([resultsFolder '/' rcFiles{cpIndex}]);
+            optDataLoader = load([resultsFolder '/' optFiles{cpIndex}]);
+            rcWindowResults(cpIndex, :) = rcDataLoader.berRCSNR;
+            optWindowResults(cpIndex, :) = optDataLoader.berSNR;
+        end
+    case {'WOLA', 'CPW'}
+        rcWindowResults = zeros(length(cpLengthVector), ...
+            length(snrValues));
+        optWindowResults = zeros(length(cpLengthVector), ...
+            length(snrValues), 6);
+        for cpIndex = 1:length(cpLengthVector)
+            rcDataLoader = load([resultsFolder '/' rcFiles{cpIndex}]);
+            optDataLoader = load([resultsFolder '/' optFiles{cpIndex}]);
+            rcWindowResults(cpIndex, :) = rcDataLoader.berRCSNR;
+            optWindowResults(cpIndex, :, 1) = optDataLoader.berSNRStep1A;
+            optWindowResults(cpIndex, :, 2) = optDataLoader.berSNRStep1B;
+            optWindowResults(cpIndex, :, 3) = optDataLoader.berSNRStep2A;
+            optWindowResults(cpIndex, :, 4) = optDataLoader.berSNRStep2B;
+            optWindowResults(cpIndex, :, 5) = optDataLoader.berSNRStep3A;
+            optWindowResults(cpIndex, :, 6) = optDataLoader.berSNRStep3B;
+        end
+end
+end
+
+
+function [rcFiles, optFiles] = separate_rc_opt(filesByType)
+rcFiles = {};
+optFiles = {};
+for fileIndex = 1:length(filesByType)
+    fileNameInfo = split(filesByType(fileIndex), '_');
+    if isequal(fileNameInfo{1}, 'rc')
+        rcFiles(end+1) = filesByType(fileIndex);  %#ok
+    else
+        optFiles(end+1) = filesByType(fileIndex);  %#ok
+    end
+end
+end
+
+
 function filesByType = file_by_type(resultFiles, typeOFDM)
 filesByType = {};
 for fileIndex = 1:length(resultFiles)
@@ -69,7 +253,7 @@ for fileIndex = 1:length(resultFiles)
         continue
     end
     fileNameInfo = split(resultFiles(fileIndex).name, '_');
-    fileTypeOFDM = fileNameInfo{2};
+    fileTypeOFDM = fileNameInfo{3};
     if isequal(fileTypeOFDM, typeOFDM)
         filesByType(end+1) = {resultFiles(fileIndex).name};  %#ok
     end
---
+++ main_interference_calculation.m
@@ -0,0 +1,360 @@
+% main_interference_calculation.m
+%   This script will perform the calculation of the ICI + ISI power for the
+%   different systems and difference cyclic prefix lengths.
+%
+% Luiz Felipe Coelho - luizfelipe.coelho@smt.ufrj.br
+% Out 17, 2022
+%
+
+
+clc
+clear
+close all
+
+
+fprintf('Starting main_interference_calculation.m ... \n\n')
+
+global folderName settingsFileName channelsFilePath
+folderName = 'optimized_windows';
+if ~isfolder(folderName)
+    error('Missing %s folder.', folderName)
+end
+settingsFileName = 'settingsData.mat';
+if ~isfile(settingsFileName)
+    error('Missing %s file.', settingsFileName)
+end
+channelsFilePath = './channels/vehA200channel2.mat';
+if ~isfile(channelsFilePath)
+    error('Missing %s file.', channelsFilePath)
+end
+
+
+typeOFDMSet = {'wtx', 'wrx', 'WOLA', 'CPW', 'CPwrx', 'CPwtx'};
+settingsLoader = load(settingsFileName);
+numSubcar = settingsLoader.settingsData.generalSettings.numberSubcarriers;
+cpLengthVector = settingsLoader.settingsData.generalSettings.cyclicPrefix;
+for typeIndex = 1:length(typeOFDMSet)
+    typeOFDM = typeOFDMSet{typeIndex};
+    tailRx = settingsLoader.settingsData.(typeOFDM).tailRx;
+    tailTx = settingsLoader.settingsData.(typeOFDM).tailTx;
+    filesByType = file_by_type(typeOFDM);
+    rcWindowInterference = zeros(length(cpLengthVector), 1);
+    switch typeOFDM
+        case {'wtx', 'CPwtx'}
+            optWindowInterference = zeros(length(cpLengthVector), 1);
+            for cpIndex = 1:length(cpLengthVector)
+                cpLength = cpLengthVector(cpIndex);
+                [csLength, ~, ~] = calculate_parameters(typeOFDM, ...
+                    cpLength, tailTx, tailRx);
+                fileName = select_by_cp(filesByType, cpLength);
+                windowTx = load([folderName '/' ...
+                    fileName]).optimizedTransmitterWindow;
+                windowRx = receiver_rc_window(numSubcar, tailRx);
+                optWindowInterference(cpIndex) = ...
+                    calculate_interference(cpLength, typeOFDM, ...
+                    windowTx, windowRx);
+            end
+        case {'wrx', 'CPwrx'}
+            optWindowInterference = zeros(length(cpLengthVector), 1);
+            for cpIndex = 1:length(cpLengthVector)
+                cpLength = cpLengthVector(cpIndex);
+                fileName = select_by_cp(filesByType, cpLength);
+                windowTx = transmitter_rc_window(numSubcar, cpLength, ...
+                    csLength, tailTx);
+                windowRx = load([folderName '/' ...
+                    fileName]).optimizedReceiverWindow;
+                optWindowInterference(cpIndex) = ...
+                    calculate_interference(cpLength, typeOFDM, ...
+                    windowTx, windowRx);
+            end
+        case {'WOLA', 'CPW'}
+            optWindowInterference = zeros(length(cpLengthVector), 6);
+            for cpIndex = 1:length(cpLengthVector)
+                cpLength = cpLengthVector(cpIndex);
+                fileName = select_by_cp(filesByType, cpLength);
+            end
+    end
+end
+
+
+function filesByType = file_by_type(typeOFDM)
+
+global folderName
+
+windowFiles = dir(fullfile(folderName));
+filesByType = {};
+for fileIndex = 1:length(windowFiles)
+    if windowFiles(fileIndex).isdir
+        continue
+    end
+    fileNameInfo = split(windowFiles(fileIndex).name, '_');
+    fileTypeOFDM = fileNameInfo{3};
+    if isequal(fileTypeOFDM, typeOFDM)
+        filesByType(end+1) = {windowFiles(fileIndex).name};  %#ok
+    end
+end
+end
+
+
+function fileName = select_by_cp(filesByType, cpLength)
+
+for fileIndex = 1:length(filesByType)
+    fileInfo = split(filesByType{fileIndex}, '_');
+    cpFile = str2double(fileInfo{end}(1:end-6));
+    if cpFile == cpLength
+        fileName = filesByType{fileIndex};
+    end
+end
+end
+
+
+function interferencePower = calculate_interference(cpLength, typeOFDM, ...
+    windowTx, windowRx)
+% Funtion to calculate interference for a given system
+%
+% - Input
+%   . cpLength: Number of samples in the cyclic prefix
+%   . typeOFDM: Type of w-OFDM system
+
+global settingsFileName channelsFilePath
+
+settingsLoader = load(settingsFileName);
+channelLoader = load(channelsFilePath);
+% System information
+numSubcar = settingsLoader.settingsData.generalSettings.numberSubcarriers;
+tailTx = settingsLoader.settingsData.(typeOFDM).tailTx;
+tailRx = settingsLoader.settingsData.(typeOFDM).tailRx;
+[csLength, prefixRemoval, circularShift] = calculate_parameters( ...
+    typeOFDM, cpLength, tailTx, tailRx);
+% Channel inforamtion
+meanChannelImpulseResponse = mean(channelLoader.vehA200channel2, 1);
+interferenceArray = array_ici_isi(meanChannelImpulseResponse, ...
+    numSubcar, cpLength, csLength, tailTx, tailRx, prefixRemoval);
+intercarrierInterference = interferenceArray(:, :, 1);
+intersymbolInterference = sum(interferenceArray(:, :, 2:end), 3);
+% Generating system matrices for calculations
+transformMatrix = dftmtx(numSubcar);
+invertTransformMatrix = transformMatrix'/numSubcar;
+circularShiftMatrix = circular_shift_matrix(numSubcar, circularShift);
+overlapAddMatrix = overlap_and_add_matrix(numSubcar, tailRx);
+removeRedundancyMatrix = remove_redundancy_matrix(numSubcar, tailRx, ...
+    prefixRemoval);
+addRedundancyMatrix = add_redundancy_matrix(numSubcar, cpLength, csLength);
+interferencePower = norm((transformMatrix*circularShiftMatrix ...
+    * overlapAddMatrix*windowRx*removeRedundancyMatrix) ...
+    * intercarrierInterference*(windowTx*addRedundancyMatrix ...
+    * invertTransformMatrix) - diag(diag((transformMatrix ...
+    * circularShiftMatrix*overlapAddMatrix*windowRx ...
+    * removeRedundancyMatrix)*intercarrierInterference*(windowTx ...
+    * addRedundancyMatrix*invertTransformMatrix))), 'fro') ...
+    + norm((transformMatrix*circularShiftMatrix*overlapAddMatrix ...
+    * windowRx*removeRedundancyMatrix)*intersymbolInterference ...
+    * (windowTx*addRedundancyMatrix*invertTransformMatrix), 'fro');
+end
+
+
+function overlapAddMatrix = overlap_and_add_matrix(numSubcar, tailRx)
+% Function to generate matrix that operates the overlap and add in the
+% samples.
+%
+% - Input:
+%   . numberSubcarriers: Number of subcarriers in the system.
+%   . tailRx: Number of samples in rise and fall tails for the receiver
+%   window.
+
+identityHalfTail = eye(tailRx/2);
+identitySubcarriers = eye(numSubcar-tailRx);
+zerosHalfTail = zeros(tailRx/2);
+zerosHalfTailSubcarriers = zeros(tailRx/2, numSubcar-tailRx);
+zerosTailSubcarriers = zeros(numSubcar-tailRx, tailRx);
+overlapAddMatrix = [zerosHalfTail identityHalfTail ...
+    zerosHalfTailSubcarriers zerosHalfTail identityHalfTail; ...
+    zerosTailSubcarriers identitySubcarriers zerosTailSubcarriers; ...
+    identityHalfTail zerosHalfTail zerosHalfTailSubcarriers ...
+    identityHalfTail zerosHalfTail];
+end
+
+
+function circularShiftMatrix = circular_shift_matrix(numSubcar, ...
+    circularShift)
+% Function to generate matrix that operates the circular shift.
+%
+% - Input:
+%   . numSubcar: Number of subcarriers in the system.
+%   . cicularShift: Samples in the circular shift.
+%
+% - Output:
+%   . circularShiftMatrix: A matrix capable of performing the circular
+%   shift operation.
+
+identityCircularShift = eye(circularShift);
+identitySubcarriersMinusCircularShift = eye(numSubcar- ...
+    circularShift);
+zerosSubcarriersMinusCircularShift = zeros(circularShift, ...
+    numSubcar-circularShift);
+circularShiftMatrix = [zerosSubcarriersMinusCircularShift' ...
+    identitySubcarriersMinusCircularShift; identityCircularShift ...
+    zerosSubcarriersMinusCircularShift];
+end
+
+
+function removeRedundancyMatrix = remove_redundancy_matrix(numSubcar, ...
+    tailRx, prefixRemoval)
+% Function to generate matrix to remove redundancy.
+%
+% - Input:
+%   . numSubcar: Number of subcarriers in the system.
+%   . tailRx: Number of samples in rise and fall tail at the receiver
+%   window.
+%   . prefixRemoval: Number of samples to be removed at the reception.
+
+removeRedundancyMatrix = [zeros(numSubcar+tailRx, prefixRemoval) ...
+    eye(numSubcar+tailRx)];
+end
+
+
+function addRedundancyMatrix = add_redundancy_matrix(numSubcar, ...
+    cpLength, csLength)
+% Function to generate the matrix that adds redundancy.
+%
+% - Input:
+%   . numSubcar: Number of subcarriers in the system.
+%   . cpLength: Number of samples in cyclic prefix.
+%   . csLength: Number of samples in cyclic suffix.
+%
+% - Output:
+%   . redundancyMatrix: Matrix that adds the cyclic redundancy to the
+%   system.
+
+identityPrefix = eye(cpLength);
+zerosPrefix = zeros(cpLength, (numSubcar-cpLength));
+identitySuffix = eye(csLength);
+zerosSuffix = zeros(csLength, (numSubcar-csLength));
+identitySubcarriers = eye(numSubcar);
+addRedundancyMatrix = [zerosPrefix identityPrefix; identitySubcarriers; ...
+    identitySuffix zerosSuffix];
+end
+
+
+function transmitterWindow = transmitter_rc_window(numSubcar, cpLength, ...
+    csLength, tailTx)
+% Function to generate the Raised Cosine transmitter window.
+%
+% - Input:
+%   . numberSubcarriers: Number of subcarriers in the system.
+%   . cyclicPrefix: Number of samples in cyclic prefix.
+%   . cyclicSuffix: Number of samples in cyclic suffix.
+%   . tailTx: Number of samples in rise and fall tail for the transmitter
+%   window.
+%
+% - Output:
+%   . transmitterWindow: Matrix with diagonal equivalent to the transmitted
+%   window.
+
+raisedCosineAxis = (-(tailTx+1)/2 + 1):1:((tailTx+1)/2 - 1);
+raisedCosine = sin(pi/2 * (.5 + raisedCosineAxis/tailTx)).^2;
+onesTransmitted = ones(1, numSubcar+cpLength+csLength ...
+    - 2*tailTx);
+windowVector = [raisedCosine onesTransmitted fliplr(raisedCosine)];
+transmitterWindow = diag(windowVector);
+end
+
+
+function receiverWindow = receiver_rc_window(numSubcar, tailRx)
+% Function to generate the Raised Cosine receiver window.
+%
+% - Input:
+%   . numberSubcarriers: Number of subcarriers in the system.
+%   . tailRx: Number of samples in rise and fall tail for the receiver
+%   window.
+
+raisedCosineAxis = (-(tailRx+1)/2+1):1:((tailRx+1)/2 - 1);
+raisedCosine = sin(pi/2 * (.5 + raisedCosineAxis/tailRx)).^2;
+onesReceived = ones(1, numSubcar-tailRx);
+windowVector = [raisedCosine onesReceived fliplr(raisedCosine)];
+receiverWindow = diag(windowVector);
+end
+
+
+function interferenceArray = array_ici_isi(channelVector, numSubcar, ...
+    cpLength, csLength, tailTx, tailRx, prefixRemoval)
+% Function to calculate the ICI and ISI array.
+%
+% - Input:
+%   . channelVector: Channel's impulse response.
+%   . numberSubcarrier: Number of subcarriers in the system.
+%   . cyclicPrefix: Number of samples in cyclic prefix.
+%   . cyclicSuffix: Number of samples in cyclic suffix.
+%   . tailTx: Number of samples in rise and fall tail at transmitter.
+%   . tailRx: Number of samples in rise and fall tail at receiver.
+%   . prefixRemoval: Number of samples (from cyclic prefix) to be removed
+%   at the receiver.
+%
+% - Output:
+%   . interferenceArray : An array with elements corresponding to the
+%   channel effects in the OFDM block, where the  
+
+channelOrder = length(channelVector) - 1;
+numberReceived = numSubcar+tailRx+prefixRemoval;
+numberTransmitted = numSubcar+cpLength+csLength;
+channelNoise = numberTransmitted-tailTx;
+dataVectorsAffected = ceil((channelOrder+tailTx)/numberReceived) + 1;
+interferenceArray = zeros(numberReceived, numberTransmitted, ...
+    dataVectorsAffected);
+for dataVectorAffected = 0:dataVectorsAffected-1
+    for receivedSample = 0:numberReceived-1
+        for transmittedSample = 0:numberTransmitted-1
+            indexer = dataVectorAffected*channelNoise ...
+                + transmittedSample - receivedSample;
+            if (0 <= indexer) && (indexer <= channelOrder)
+                interferenceArray(receivedSample+1, ...
+                    transmittedSample+1, dataVectorAffected+1) ...
+                    = channelVector(indexer+1);
+            end
+        end
+    end
+end
+end
+
+
+function [csLength, prefixRemoval, circularShift] = ...
+    calculate_parameters(typeOFDM, cpLength, tailTx, tailRx)
+% Function to calculate parameters for each w-OFDM system
+%
+% - Input:
+%   . typeOFDM: w-OFDM system
+%   . cyclicPrefix: Number of samples in the cyclic prefix
+%   . tailTx: Number of samples in rise and fall tail for Tx
+%   . tailRX: Number of samples in rise and fall tail for Rx
+
+switch typeOFDM
+    case 'wtx'
+        csLength = tailTx;
+        prefixRemoval = cpLength;
+        circularShift = 0;
+    case 'wrx'
+        csLength = tailRx/2;
+        prefixRemoval = cpLength - tailRx/2;
+        circularShift = 0;
+    case 'WOLA'
+        csLength = tailTx;
+        prefixRemoval = cpLength - tailRx;
+        circularShift = tailRx/2;
+    case 'CPW'
+        csLength = tailTx + tailRx/2;
+        prefixRemoval = cpLength - tailRx/2;
+        circularShift = 0;
+    case 'CPwtx'
+        csLength = 0;
+        prefixRemoval = cpLength - tailTx;
+        circularShift = tailTx;
+    case 'CPwrx'
+        csLength = 0;
+        prefixRemoval = cpLength - tailRx;
+        circularShift = tailRx/2;
+end
+end
+
+
+% EoF
+
